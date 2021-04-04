4f8eb5eb806e4915bcf03b1407ea48827cc9e8c7
jagritmalhotra2507 <74820002+jagritmalhotra2507@users.noreply.github.com>
2021-04-04 16:36:57 +0530

    Add files via upload

---
+++ js/slingShot.js
@@ -0,0 +1,37 @@
+class SlingShot{
+
+constructor(bodyA,pointB){
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness :0.004,
+        length: 1
+    }
+   
+this.bodyA=bodyA
+this.pointB=pointB
+this.slingshot=Constraint.create(options)
+World.add(world,this.slingshot)
+}
+fly (){
+    this.slingshot.bodyA=null;
+
+}
+attach(body){
+
+    this.slingshot.bodyA=body
+
+}
+display(){
+
+    if(this.slingshot.bodyA){
+        var pointA=this.bodyA.position
+        var pointB=this.pointB
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+
+    }
+
+}
+}
